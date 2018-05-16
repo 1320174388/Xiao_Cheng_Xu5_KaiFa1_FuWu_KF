b6c984f877baca2d9a86e2e6ff2f018177ee524a
shenhao <1528274382@QQ.com>
2018-05-16 23:32:04 +0800

    20180616:23:31_预约页面完成

---
+++ client/pages/product/product.js
@@ -0,0 +1,116 @@
+// pages/product/product.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 图片和名称
+    imgg: [{
+      imgUrl: "./../img/2.png",
+      text: "美肤液"
+    }, {
+        imgUrl: "./../img/3.png",
+      text: "美容液"
+    }, {
+        imgUrl: "./../img/4.png",
+      text: "精华霜"
+    }, {
+        imgUrl: "./../img/5.png",
+      text: "口红"
+    }, {
+        imgUrl: "./../img/6.png",
+      text: "卸妆水"
+    }, {
+        imgUrl: "./../img/7.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }, {
+      imgUrl: "./../img/8.png",
+      text: "粉底液"
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+
+})
